fd2ed0207a5f665b522c2972b59f1cc856cb442a
Camila Neiza <castronjulianac@javeriana.edu.co>
2025-04-06 00:37:49 -0500

    Página sin iniciar sesión

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <header class="navbar">
+      <div class="logo">MovieNest</div>
+      <nav class="nav-links">
+        <router-link to="/Iniciar Sesión" class="nav-item">Sign In</router-link>
+        <router-link to="/Crear cuenta" class="nav-item">Create Account</router-link>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: "NavigationBar",
+  };
+  </script>
+  
+  <style>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 40px 32px;
+    background-color: transparent;
+    color: white; /* Cambia el color según tu diseño */
+  }
+  
+  .logo {
+    font-size: 24px;
+    font-weight: bold;
+  }
+  
+  .nav-links {
+    display: flex;
+  }
+  
+  .nav-item {
+    margin-left: 20px;
+    text-decoration: none;
+    color: white; /* Cambia el color según tu diseño */
+    transition: color 0.3s;
+  }
+  
+  .nav-item:hover {
+    color: #00bcd4; /* Color de hover */
+  }
+  </style>
+  
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="page-container1">
+        <div class="content">
+            <Header />
+        </div>
+     
+      
+    </div>
+
+    <div class="page-container2">
+     
+      <div class="content">
+        <img src="/src/assets/imagen1-inicio.png" alt="Descripción de la imagen" class="main-image" />
+      </div>
+    </div>
+
+    <div class="page-container3">
+      <div class="content">
+        <img src="/src/assets/imagen2-inicio.png" alt="Descripción de la imagen" class="main-image" />
+      </div>
+    </div>
+
+    <div class="page-container-end">
+      <div class="content">
+        <img src="/src/assets/club-pelea.jpg" alt="Descripción de la imagen" class="main-image" />
+        <img src="/src/assets/club-pelea.jpg" alt="Descripción de la imagen" class="secondary-image" />
+      </div>
+    </div>
+
+</template>
+
+  
+  <script>
+  import Header from '@/components/Header.vue';
+  
+  export default {
+    components: {
+      Header,
+    },
+  }
+  </script>
+  
+  <style scoped>
+  .page-container1 {
+    flex-direction: column;
+    margin: 0px;
+    padding: 20vw;
+    align-items: center;
+    background-image: url('/src/assets/club-pelea.jpg');
+    background-size: cover;
+    background-position: center; /* Color de fondo similar al de Letterboxd */
+  }
+
+    .page-container2 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-image: url('/src/assets/club-pelea.jpg');
+        background-size: cover;
+        background-position: center; /* Color de fondo similar al de Letterboxd */
+    }
+
+    .page-container3 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-image: url('/src/assets/club-pelea.jpg');
+        background-size: cover;
+        background-position: center; /* Color de fondo similar al de Letterboxd */
+    }
+
+    .page-container-end {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-image: url('/src/assets/club-pelea.jpg');
+        background-size: cover;
+        background-position: center; /* Color de fondo similar al de Letterboxd */
+    }
+
+  .content {
+    max-width: 800px;
+    text-align: center;
+    margin: 0px;
+  }
+  
+  .main-image {
+    width: 100%;
+    height: auto;
+  }
+  
+  .secondary-image {
+    width: 80%;
+    height: auto;
+    margin-top: 20px;
+  }
+  </style>
