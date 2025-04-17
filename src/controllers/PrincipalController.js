31f228b4f1ea0eaa5b8e18f2fad966a9f06437a1
Juanba2002 <bajuan00@gmail.com>
2025-04-16 21:05:02 -0500

    principal

--- src/controllers/PrincipalController.js
+++ src/controllers/PrincipalController.js
@@ -1,6 +1,6 @@
 import apolloClient from '@/models/apollo/apollo'
-import gql from 'graphql-tag'
 import HeaderSearch from '@/views/components/HeaderSearch.vue'
+import gql from 'graphql-tag'
 
 const ADD_TO_FAVORITES = gql`
   mutation AddFavoriteMovie($user_id: ID!, $movie_id: ID!) {
@@ -12,10 +12,10 @@ const ADD_TO_FAVORITES = gql`
 `
 
 const ADD_TO_WATCH_LATER = gql`
-  mutation AddToWatchLater($userId: ID!, $movieId: ID!) {
-    addToWatchLater(userId: $userId, movieId: $movieId) {
-      userId
-      movieId
+  mutation AddToWatchLater($user_id: ID!, $movie_id: ID!) {
+    addToWatchLater(user_id: $user_id, movie_id: $movie_id) {
+      success
+      movie_id
     }
   }
 `
@@ -89,7 +89,7 @@ export default {
         console.error('No se encontró el ID de usuario.')
         return
       }
-    
+
       try {
         await apolloClient.mutate({
           mutation: ADD_TO_FAVORITES,
@@ -114,7 +114,10 @@ export default {
       try {
         await apolloClient.mutate({
           mutation: ADD_TO_WATCH_LATER,
-          variables: { userId, movieId }
+          variables: {
+            user_id: userId,
+            movie_id: movieId, // ya no necesita parseInt
+          },
         })
         console.log('Película agregada a ver más tarde')
       } catch (error) {
--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,58 +1,84 @@
-import apolloClient from '@/models/apollo/apollo' 
-
-import gql from 'graphql-tag'
+import apolloClient from '@/models/apollo/apollo'
 import HeaderSearch from '@/views/components/HeaderSearch.vue'
+import gql from 'graphql-tag'
 
 export default {
-    name: 'ProfileView',
-    components: { HeaderSearch },
-    async mounted() {
-        await this.fetchFavorites()
-        await this.fetchWatchLater()
-      },
-      methods: {
-        async fetchFavorites() {
-          try {
-            const response = await apolloClient.query({
-              query: gql`
-                query GetFavorites {
-                  favorites {
-                    id
-                    poster
-                  }
-                }
-              `
-            })
-            this.favoriteMovies = response.data.favorites.map(movie => ({
-              id: movie.id,
-              title: movie.title,
-              poster: movie.poster ? `https://image.tmdb.org/t/p/w500${movie.poster}` : '/placeholder.jpg'
-            }))
-          } catch (e) {
-            console.error('Error loading favorites:', e)
-          }
-        },
-    
-        async fetchWatchLater() {
-          try {
-            const response = await apolloClient.query({
-              query: gql`
-                query GetWatchLater {
-                  watchLater {
-                    id
-                    poster
-                  }
-                }
-              `
-            })
-            this.watchLaterMovies = response.data.watchLater.map(movie => ({
-              id: movie.id,
-              title: movie.title,
-              poster: movie.poster ? `https://image.tmdb.org/t/p/w500${movie.poster}` : '/placeholder.jpg'
-            }))
-          } catch (e) {
-            console.error('Error loading watch later:', e)
-          }
+  name: 'ProfileView',
+  components: { HeaderSearch },
+
+  data() {
+    return {
+      favoriteMovies: [],
+      watchLaterMovies: []
+    }
+  },
+
+  async mounted() {
+    await this.fetchFavorites()
+    await this.fetchWatchLater()
+  },
+
+  methods: {
+    async fetchFavorites() {
+      try {
+        const userId = localStorage.getItem('userId')
+        if (!userId) {
+          console.warn('No hay userId en localStorage')
+          return
         }
+
+        const response = await apolloClient.query({
+          query: gql`
+            query GetFavorites($user_id: ID!) {
+              getFavoriteMovies(user_id: $user_id) {
+                id
+                poster
+              }
+            }
+          `,
+          variables: {
+            user_id: userId
+          }
+        })
+
+        this.favoriteMovies = response.data.getFavoriteMovies.map(movie => ({
+          id: movie.id,
+          poster: movie.poster || '/placeholder.jpg'
+        }))
+      } catch (e) {
+        console.error('❌ Error loading favorites:', e)
       }
+    },
+
+    async fetchWatchLater() {
+      try {
+        const userId = localStorage.getItem('userId')
+        if (!userId) {
+          console.warn('No hay userId en localStorage')
+          return
+        }
+
+        const response = await apolloClient.query({
+          query: gql`
+            query GetWatchLater($user_id: ID!) {
+              getWatchLaterMovies(user_id: $user_id) {
+                id
+                poster
+              }
+            }
+          `,
+          variables: {
+            user_id: userId
+          }
+        })
+
+        this.watchLaterMovies = response.data.getWatchLaterMovies.map(movie => ({
+          id: movie.id,
+          poster: movie.poster || '/placeholder.jpg'
+        }))
+      } catch (e) {
+        console.error('❌ Error loading watch later:', e)
+      }
+    }
+  }
 }
